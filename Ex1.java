8e3eae46e55efabe531d4588cc8a6e5fa99d8150
JoanGarcia8 <joan.garcia@institutvidreres.cat>
2021-11-17 11:10:22 +0100

    commit 3

--- Ex1.java
+++ Ex1.java
@@ -1,3 +1,15 @@
-public class Ex1 {
-    
+import java.util.Scanner;
+ 
+class Ex1 {
+    public static void main(String[]args){
+    Scanner kb = new Scanner (System.in);
+    System.out.println("Posa un numero");
+    int numero = kb.nextInt();
+    while (numero!=0){
+            System.out.println("Els seu doble es: " +(numero*2));
+            System.out.println("Posa un numero");
+            numero = kb.nextInt();
+    }
 }
+}
+
