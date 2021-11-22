b58231ad960e42cabed14e623d84f0873b8a4cbe
JoanGarcia8 <joan.garcia@institutvidreres.cat>
2021-11-22 22:23:06 +0100

    commit ej8/variables

--- javascript.js
+++ javascript.js
@@ -1,13 +1,19 @@
-//6. Vaig probar el tofixed pero no sabia on possar-ho ni la manera
-var number = 1.234
-var BigInt = 5.678
-var suma = (number + BigInt)
-var resta = (number - BigInt)
-var divisio = (number / BigInt)
-var multiplicacio = (number*BigInt)
-console.log(suma)
-console.log(resta)
-console.log(divisio)
-console.log(multiplicacio)
+// 6. Vaig probar el tofixed pero no sabia on possar-ho ni la manera
+// var number = 1.234
+// var BigInt = 5.678
+// var suma = (number + BigInt)
+// var resta = (number - BigInt)
+// var divisio = (number / BigInt)
+// var multiplicacio = (number*BigInt)
+// console.log(suma)
+// console.log(resta)
+// console.log(divisio)
+// console.log(multiplicacio)
+
+// 8.
+var numero = 10
+var string = 'Bondia'
+var junt = (numero/string)
+console.log(junt)
 
 
