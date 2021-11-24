7e86048f4e6f669f601673a76bc9403d1edb1dae
JoanGarcia8 <joan.garcia@institutvidreres.cat>
2021-11-24 11:05:34 +0100

    commit ej1/logica

---
+++ Logica/logica.js
@@ -0,0 +1,16 @@
+// 1.
+var numero = prompt('Introdueix un numero: ')
+if (( numero % 3 ) == 0){
+
+	alert(numero + ' es multiplo de 3');
+    
+}else if ((numero % 7) == 0){
+
+	alert(numero + ' es multiple de 7');
+}
+
+else{
+    alert('El numero no es multiple de 3 ni de 7');
+}
+
+
